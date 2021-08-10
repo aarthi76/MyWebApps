f978a84c2bf577836e3ff8fa7ffc19ddb914ca5e
Aarthi <52964048+aarthi76@users.noreply.github.com>
2021-08-10 12:52:15 +0530

    Add files via upload

---
+++ SimpleStockPriceSC.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 12:26:59 2021
+
+@author: Aarthi
+"""
+
+import yfinance as yf
+import streamlit as st
+#import pandas as pd
+st.write("""
+         # Simple Stock Price App
+         
+         #### Shown are the stock closing price and volume of Google!
+         
+         """)
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start = '2010-8-10', end = '2021-8-10')
+# Open  High  Low  Close  Volume Dividends  Stock Splits 
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume) 
